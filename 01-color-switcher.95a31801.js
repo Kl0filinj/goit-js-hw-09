a1e6f62e0faee0885ce6a6555327f8c5b2fede90
Kl0filinj <101584452+Kl0filinj@users.noreply.github.com>
2022-07-24 08:45:49 +0000

    Deploying to gh-pages from @ Kl0filinj/goit-js-hw-09@006c4daa6e0fcabfadb0b7fa05ee3509672afee9 🚀

---
+++ 01-color-switcher.95a31801.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyRef:document.body};let e=!1,o=null;t.startBtn.addEventListener("click",(()=>{e||(e=!0,t.startBtn.setAttribute("disabled",!0),o=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;console.log(e),t.bodyRef.style.backgroundColor=`${e}`}),1e3))})),t.stopBtn.addEventListener("click",(()=>{clearInterval(o),t.startBtn.removeAttribute("disabled",!0),e=!1}));
+//# sourceMappingURL=01-color-switcher.95a31801.js.map
